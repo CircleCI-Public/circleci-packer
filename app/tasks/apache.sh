d495ea537b3d31668b697dcc7701d50431522011
Simon McCartney <simon.mccartney@taskize.com>
2018-06-26 08:07:17 +0100

    Use simple apache2 setup on app

--- app/tasks/apache.sh
+++ app/tasks/apache.sh
@@ -4,7 +4,7 @@ set -e
 echo '---- install Apache'
 
 DEBIAN_FRONTEND=noninteractive apt-get -y update
-DEBIAN_FRONTEND=noninteractive apt-get -y install apache
+DEBIAN_FRONTEND=noninteractive apt-get -y install apache2
 
 cat > /var/www/html/index.html <<HERE
 Plain text FTW!
